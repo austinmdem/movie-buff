fba6677b445fc49b64acae3eaaad9ea55c007515
Austin Deming <austindeming@gmail.com>
2020-08-22 15:43:04 -0700

    deploy to heroku

--- server.js
+++ server.js
@@ -19,11 +19,7 @@ app.use('/api/posts', require('./routes/api/posts'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-  });
+  app.use(express.static('src/build'));
 }
 
 const PORT = process.env.PORT || 5000;
---
+++ src/build/precache-manifest.6913cba49110747d8ed1b62404ac8d4a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a6a179b7c1d0cb90ecf29a35b27c0c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb9f5171a9d59cc31e1a",
+    "url": "/static/css/main.c56a06a0.chunk.css"
+  },
+  {
+    "revision": "81132130d634a6a9d8f4",
+    "url": "/static/js/2.50923e6e.chunk.js"
+  },
+  {
+    "revision": "3a7685355c5a7477e37b3a8f47f918b2",
+    "url": "/static/js/2.50923e6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb9f5171a9d59cc31e1a",
+    "url": "/static/js/main.b790413c.chunk.js"
+  },
+  {
+    "revision": "6f04b2d71f1a8f1ccbc7",
+    "url": "/static/js/runtime-main.c20aae40.js"
+  },
+  {
+    "revision": "667f060fa659a698d91b807fe7bb6264",
+    "url": "/static/media/moviebg.667f060f.jpg"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
